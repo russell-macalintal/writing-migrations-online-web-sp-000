36abed3a6a3e29d7fc236f87d3cf9ce09ab41848
Russell Macalintal <russell.macalintal@gmail.com>
2020-01-13 04:41:59 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,4 +1,4 @@
-class AddGradeAndBirthdateToStudents < ActiveRecord::Migrate[5.1]
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change
     add_column :students, :grade, :integer
     add_column :studnets, :birthdate, :string
