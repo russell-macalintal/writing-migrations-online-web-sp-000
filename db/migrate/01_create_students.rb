30116ac0718ec0bf1798f0822984ed9fcafc04ab
Russell Macalintal <russell.macalintal@gmail.com>
2020-01-13 04:32:11 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,7 +1,7 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
   def change
-    create_table :students {|t|
+    create_table :students do |t|
       t.string :name
-    }
+    end
   end
 end
